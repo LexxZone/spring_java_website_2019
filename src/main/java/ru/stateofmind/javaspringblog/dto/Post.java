17933d94d1f552c4825aebfa1123aa6961f67dbe
Alexey Dvoryaninov <alexey.dvoryaninov@corp.codeinside.ru>
2019-02-26 19:13:11 +0300

    Init commit

---
+++ src/main/java/ru/stateofmind/javaspringblog/dto/Post.java
@@ -0,0 +1,9 @@
+package ru.stateofmind.javaspringblog.dto;
+
+import lombok.Data;
+
+@Data
+public class Post {
+	private String title;
+	private String body;
+}
---
+++ src/main/resources/templates/hello.ftl
@@ -0,0 +1,8 @@
+<#import "main.ftl" as main>
+
+<@main.main>
+
+<h1>Hey, you!</h1>
+
+</@main.main>
+
