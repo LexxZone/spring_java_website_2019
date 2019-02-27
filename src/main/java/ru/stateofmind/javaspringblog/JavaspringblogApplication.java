cfc46847a58d428a5c2551bbf2cb4dd401ffb9ad
Alexey Dvoryaninov <alexey.dvoryaninov@corp.codeinside.ru>
2019-02-27 09:36:02 +0300

    Add all files to tracking

---
+++ src/main/java/ru/stateofmind/javaspringblog/JavaspringblogApplication.java
@@ -0,0 +1,13 @@
+package ru.stateofmind.javaspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringblogApplication.class, args);
+	}
+
+}
